6166658556cc873550384be206041a3caba0c477
sally al sardouk <sally.sardouk98@gmail.com>
2021-02-12 10:13:09 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
                 onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
